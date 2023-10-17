c10dd4b3cbdac803baab63fa273b6cbfb571b729
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2023-10-17 12:38:43 +0200

    Init configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      backdropBlur: {
+        theme: "10px",
+      },
     },
   },
   plugins: [],
